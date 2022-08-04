a3d4aa1cf48d1bde133f5c1cc14f4a5e019b4738
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-04 12:50:17 +0100

    checking push

---
+++ roboMilk.js
@@ -0,0 +1,41 @@
+function getMilk(budget, costPerBottle) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+
+
+
+  console.log("buy "+ calcBottles(budget, costPerBottle) +" bottles of milk")
+
+
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+  return calcChange(budget, costPerBottle);
+
+}
+
+function calcBottles(startingMoney, costPerBottle){
+  var numberOfBottles = Math.floor(startingMoney / costPerBottle);
+  return numberOfBottles;
+}
+
+function calcChange(startingAmount, costPerBottle) {
+  var change = startingAmount % costPerBottle;
+  return change;
+}
+
+
+console.log("hello master, here is your "+ getMilk(5,1.5) + " change");
