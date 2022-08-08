2dad50ac1e0e1d094e94f24e1e677645dea36b2e
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-08 10:24:35 +0100

    final love calc push

---
+++ loveCalculator.js
@@ -0,0 +1,5 @@
+var yourName = prompt("Enter your name : ");
+var partnerName = prompt("Enter your partners name : ");
+var n = Math.random();
+n = Math.floor(n*100) +1;
+alert("your compatability is "+n+"%");
