008ebe3f127a3d807a1992964cdbf47086a8ba3c
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-08 16:49:50 +0100

    bmi calculator now includes if statements

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,6 +1,14 @@
 function bmiCalculator(weight, height) {
   var bmi = Math.floor(weight / (Math.pow(height, 2)));
-  return bmi;
+
+  if (bmi < 18.5) {
+    var interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+  } else if (bmi > 18.5 && bmi < 24.9) {
+    var interpretation = "Your BMI is " + bmi + ", so you have normal weight.";
+  } else if (bmi > 24.9) {
+    var interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+  }
+  return interpretation;
 }
-var bmi = bmiCalculator(65, 1.8);
-console.log(bmi);
+var interpretation = bmiCalculator(80, 2);
+console.log(interpretation);
--- loveCalculator.js
+++ loveCalculator.js
@@ -2,4 +2,10 @@ var yourName = prompt("Enter your name : ");
 var partnerName = prompt("Enter your partners name : ");
 var n = Math.random();
 n = Math.floor(n*100) +1;
+
+if(n > 70){
+  alert("your compatability is "+n+"% you are a perfect match");
+}
+else{
 alert("your compatability is "+n+"%");
+}
