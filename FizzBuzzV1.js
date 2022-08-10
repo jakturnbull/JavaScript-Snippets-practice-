7b52986767e80a98ebee63dd684b3a40b301099c
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-10 20:52:11 +0100

    final save

--- FizzBuzzV1.js
+++ FizzBuzzV1.js
@@ -10,7 +10,7 @@ function fizzBuzz(){
   else if(number%3 == 0 && number%5 == 0){
       output.push("FizzBuzz");
   }
-  if(number%3 != 0 && number%5 != 0){
+  else if(number%3 != 0 && number%5 != 0){
       output.push(number);
   }
   number++;
