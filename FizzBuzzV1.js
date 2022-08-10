7ce28d38535e759c05d945af3516cd91701c017f
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-10 20:13:38 +0100

    fizzbuzz without a while loop

--- FizzBuzzV1.js
+++ FizzBuzzV1.js
@@ -1,9 +1,50 @@
 var output = [];
 var number = 1;
 function fizzBuzz(){
-  output.push(number);
-number++;
+  if(number % 3 == 0 && number % 5 != 0){
+      output.push("Fizz");
+    }
+  if(number%3 != 0 && number%5 == 0){
+      output.push("Buzz");
+  }
+  if(number%3 == 0 && number%5 == 0){
+      output.push("FizzBuzz");
+  }
+  if(number%3 != 0 && number%5 != 0){
+      output.push(number);
+  }
+  number++;
 
 
 console.log(output);
 }
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
+      fizzBuzz()
