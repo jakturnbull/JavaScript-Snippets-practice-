d9babee0d2dd5689f1b4ed98fc1573e9ab31d5cb
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-10 20:32:56 +0100

    added else to if statements

--- FizzBuzzV1.js
+++ FizzBuzzV1.js
@@ -4,10 +4,10 @@ function fizzBuzz(){
   if(number % 3 == 0 && number % 5 != 0){
       output.push("Fizz");
     }
-  if(number%3 != 0 && number%5 == 0){
+  else if(number%3 != 0 && number%5 == 0){
       output.push("Buzz");
   }
-  if(number%3 == 0 && number%5 == 0){
+  else if(number%3 == 0 && number%5 == 0){
       output.push("FizzBuzz");
   }
   if(number%3 != 0 && number%5 != 0){
