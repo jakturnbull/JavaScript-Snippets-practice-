adc258155b80e7a883103d7cdb8d4f3ed98448ac
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-11 21:13:09 +0100

    who is paying for lunch solution coded and correct

---
+++ whosPaying.js
@@ -0,0 +1,13 @@
+function whosPaying(names) {
+
+/******Don't change the code above*******/
+var nameArray = ["Angela", "Ben", "Jenny", "Michael", "Chloe"];
+var whoPaysID = Math.round( (Math.random()*nameArray.length));
+var names = nameArray[whoPaysID]+"is going to buy lunch today!";
+return names;
+
+
+
+/******Don't change the code below*******/
+}
+whosPaying();
