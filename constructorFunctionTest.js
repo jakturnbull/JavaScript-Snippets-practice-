80dc2e88f56cb2b04ab51f3210aee014782ecf5f
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-25 21:38:47 +0100

    TESTING OUT CREATING A CONSTRUCTOR FUNCTION

---
+++ constructorFunctionTest.js
@@ -0,0 +1,8 @@
+function HouseKeeper(name,age,rightToWork,willingToClean){
+  this.name = name;
+  this.age = age;
+  this.rightToWork = rightToWork;
+  this.willingToClean = willingToClean;
+}
+var houseKeeper1 = new HouseKeeper("jan", 19, true , ["toilets","bedrooms","windows"]);
+console.log(houseKeeper1);
