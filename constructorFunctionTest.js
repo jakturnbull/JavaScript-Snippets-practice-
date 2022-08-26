094145594db173b488c1e1d1bfd474c053b5669b
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-08-26 19:01:27 +0100

    added function to constructor functions

--- constructorFunctionTest.js
+++ constructorFunctionTest.js
@@ -3,6 +3,7 @@ function HouseKeeper(name,age,rightToWork,willingToClean){
   this.age = age;
   this.rightToWork = rightToWork;
   this.willingToClean = willingToClean;
+  this.clean = function(){console.log("cleaning in progress");}
 }
 var houseKeeper1 = new HouseKeeper("jan", 19, true , ["toilets","bedrooms","windows"]);
-console.log(houseKeeper1.name);
+houseKeeper1.clean();
