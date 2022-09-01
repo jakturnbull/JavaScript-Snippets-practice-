e4a7eb34235b55170e242a56892787ef46447357
Jak-Turnbull <jak.turnbull1@gmail.com>
2022-09-02 00:00:43 +0100

    twoSum leetcode challenge

---
+++ twoSum.js
@@ -0,0 +1,16 @@
+
+
+
+function twoSum(nums, target) {
+var number=[];
+  for(var i = 0; i <=nums.length-1; i++){
+    for(var j = i ; j <=nums.length-1; j++){
+      if ( nums[i]+nums[j+1] == target){
+        number.push(i, j+1);
+      }
+    }
+
+  }
+  return number;
+};
+twoSum([2,7,11,15],9);
